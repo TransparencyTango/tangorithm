64ee7e6f8650ef236ec64a86f140a96b27fcbc1b
Nina Matthias <luhlain@gmail.com>
2018-12-16 21:24:25 +0100

    simple native language visualization (Caras prototype)

---
+++ Datadazed/Datadazed/Assets/Scenes/Scripts/LanguageButtons.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class LanguageButtons : MonoBehaviour {
+
+	public Button chineseButton, germanButton, englishButton;
+	private GameObject facesPanel;
+	private Image[] foundImages;
+
+	void Start () {
+		chineseButton.onClick.AddListener(delegate { ShowFaces("Chinese"); });
+		germanButton.onClick.AddListener(delegate { ShowFaces("German"); });
+		englishButton.onClick.AddListener(delegate { ShowFaces("English"); });
+
+		facesPanel = this.gameObject;
+		foundImages = facesPanel.GetComponentsInChildren<Image>(true);
+	}
+
+	void ShowFaces(string language)
+	{
+		for (int i = 0; i < foundImages.Length; i++)
+		{
+			GameObject image = foundImages[i].gameObject;
+			if (image.tag == language)
+			{
+				image.SetActive(true);
+			}
+			else
+			{
+				image.SetActive(false);
+			}
+		}
+	}
+}
---
+++ Datadazed/Datadazed/Assets/Scenes/Scripts/PageController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PageController : MonoBehaviour {
+
+	public GameObject[] panelPages;
+	public Button seeWhyButton;
+	
+
+	// Use this for initialization
+	void Start () {
+		seeWhyButton.onClick.AddListener(() => SwitchPanelTask(0,1));
+	}
+	
+	void SwitchPanelTask(int current, int next)
+	{
+		panelPages[current].SetActive(false);
+		panelPages[next].SetActive(true);
+	}
+}
