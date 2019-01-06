51f0d9b7abd4f2e1ff1fe7f6719f44dbccd77db7
Nina <ninamatthias@gmx.de>
2019-01-06 23:28:07 +0100

    The Mirror displays a stickman which can be mannipulated from App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,33 +1,55 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
+import Mirror from './Mirror.js';
 
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 const Start = () => <h2>Who are you?</h2>;
-const Mirror = () => <h2>That is you</h2>;
 const VisualizeResults = () => <h2>That is why it is you</h2>;
 
-const AppRouter = () => (
-  <Router>
-    <div>
-      <nav>
-        <ul>
-          <li>
-            <Link to="/">Start</Link>
-          </li>
-          <li>
-            <Link to="/mirror/">Mirror</Link>
-          </li>
-          <li>
-            <Link to="/visualization/">Explanation</Link>
-          </li>
-        </ul>
-      </nav>
+class AppRouter extends Component {
 
-      <Route path="/" exact component={Start} />
-      <Route path="/mirror/" component={Mirror} />
-      <Route path="/visualization/" component={VisualizeResults} />
-    </div>
-  </Router>
-);
+  constructor() {
+    super();
+    this.state = {
+      person: "unknown",
+    }
+
+    this.receivePersonData = this.receivePersonData.bind(this);
+  }
+
+  receivePersonData = (personValue) => {
+      this.setState({person: personValue});
+  }
+
+  render() {
+    return (
+      <Router>
+        <div>
+          <nav>
+            <ul>
+              <li>
+                <Link to="/">Start</Link>
+              </li>
+              <li>
+                <Link to="/mirror/">Mirror</Link>
+              </li>
+              <li>
+                <Link to="/visualization/">Explanation</Link>
+              </li>
+            </ul>
+          </nav>
+
+          <Route path="/" exact component={Start} />
+          <Route
+                path="/mirror"
+                component={() => <Mirror  person={this.state.person}/>}
+          />
+          <Route path="/visualization/" component={VisualizeResults} />
+        </div>
+      </Router>
+    );
+  }
+}
+
 export default AppRouter;
---
+++ frontend/src/Mirror.js
@@ -0,0 +1,24 @@
+import React, { Component } from 'react';
+
+var pics = {
+  "unknown":  require('./img/unknown_person.jpg'),
+  "pink":     require('./img/pink_hair.jpg'),
+  "green":    require('./img/green_hair.jpg'),
+};
+
+class Mirror extends Component {
+
+  render() {
+    return (
+      <div>
+        <h2>That is you</h2>
+        <p>
+          <img src={pics[this.props.person]} alt="" />
+        </p>
+      </div>
+    );
+  }
+
+}
+
+export default Mirror;
