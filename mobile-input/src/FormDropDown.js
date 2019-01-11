d283ca6d46bf5abc495c88fe70db506e63943ee9
Nina <ninamatthias@gmx.de>
2019-01-11 21:29:43 +0100

    form elements can be stateless

--- mobile-input/src/FormDropDown.js
+++ mobile-input/src/FormDropDown.js
@@ -1,33 +1,11 @@
 import React from 'react';
 
-class FormDropDown extends React.Component {
-
-  constructor(props) {
-    super(props);
-    this.state = {
-      name: this.props.name,
-      options: this.props.options,
-      value: this.props.value
-    }
-    this.handleChange = this.handleChange.bind(this);
-  }
-
-  handleChange(event) {
-    this.setState({
-      value: event.target.value
-    });
-    this.props.handleChange(event);
-  }
-
-  render() {
-    return (
-      <React.Fragment>
-      <label>{this.state.name}:</label>
-      <select name={this.state.name} value={this.state.value} onChange={this.handleChange}>
-      {this.state.options.map((item) => <option key={item}> {item} </option>)}
-      </select>
-      </React.Fragment>
-    );
-  }
-}
+ const FormDropDown = (props) => (
+  <React.Fragment>
+    <label>{props.name}:</label>
+    <select name={props.name} value={props.value} onChange={(event) => props.handleChange(event)}>
+      {props.options.map((item) => <option key={item}> {item} </option>)}
+    </select>
+  </React.Fragment>
+  );
 export {FormDropDown};
