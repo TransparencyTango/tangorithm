5f8317ef63bc7f38d44fd50b181bbff56555c64f
Eva-Maria Herbst <emherbst@web.de>
2019-01-20 11:46:54 +0100

    switch between different models

--- frontend/src/Modeldemo.js
+++ frontend/src/Modeldemo.js
@@ -3,45 +3,45 @@ import * as BABYLON from 'babylonjs';
 import BabylonScene from './SceneComponent.js';
 import 'babylonjs-loaders';
 
-//isImportTest = true
-//will import the nurse, exported from .dae to .babylon
-const isImportTest = true;
+//const importCase = "NurseFromDae";
+//else we will see the dude
+const importCase = "NurseFromDae";
 
 class PageWithScene extends React.Component<{}, {}> {
   constructor() {
     super();
-    if (isImportTest) {
-      this.showImportTest = this.showImportTest.bind(this);
-    }
-    else{
-      this.showReflection = this.showReflection.bind(this);
+    switch (importCase) {
+      case "NurseFromDae":
+        this.showImportTest = this.showImportTest.bind(this);
+        break;
+      default:
+        this.showReflection = this.showReflection.bind(this);
     }
   }
   
-  prepareCamera(){   
-    if (isImportTest) {
-      this.camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 0, 0), this.scene);
-      this.camera.radius = 3;
-      this.camera.heightOffset = 2;
-      this.camera.rotationOffset = 180;
-      this.camera.cameraAcceleration = 0.05
-      this.camera.maxCameraSpeed = 10
-      this.camera.attachControl(this.canvas, true);
-    }
-    else {
-      this.camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 0, 0), this.scene);
-      this.camera.radius = 160;
-      this.camera.heightOffset = 100;
-      this.camera.rotationOffset = 0;
-      this.camera.cameraAcceleration = 0.05
-      this.camera.maxCameraSpeed = 10
-      this.camera.attachControl(this.canvas, true);
-      
-      //the sphere will be at the dude's head position
-      this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, this.scene);   
-    }
-    
-    
+  prepareCamera(){
+    switch (importCase) {
+      case "NurseFromDae":
+        this.camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 0, 0), this.scene);
+        this.camera.radius = 3;
+        this.camera.heightOffset = 2;
+        this.camera.rotationOffset = 180;
+        this.camera.cameraAcceleration = 0.05
+        this.camera.maxCameraSpeed = 10
+        this.camera.attachControl(this.canvas, true);
+        break;
+      default:
+        this.camera = new BABYLON.FollowCamera("FollowCam", new BABYLON.Vector3(0, 0, 0), this.scene);
+        this.camera.radius = 160;
+        this.camera.heightOffset = 100;
+        this.camera.rotationOffset = 0;
+        this.camera.cameraAcceleration = 0.05
+        this.camera.maxCameraSpeed = 10
+        this.camera.attachControl(this.canvas, true);
+        
+        //the sphere will be at the dude's head position
+        this.sphere = BABYLON.Mesh.CreateSphere("sphere1", 16, 2, this.scene);
+    }    
   }
   
   showImportTest = (newMeshes, particleSystems, skeletons) => {
@@ -62,15 +62,16 @@ class PageWithScene extends React.Component<{}, {}> {
     this.scene = scene;
     
     this.prepareCamera();
-
-    if (isImportTest) {
-      this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, -100), this.scene);
-
-      BABYLON.SceneLoader.ImportMesh("", "/models/nurseFromDae/", "nurse.babylon", this.scene, this.showImportTest);
-    }
-    else {
-      this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, 100), this.scene);
-      BABYLON.SceneLoader.ImportMesh("", "/models/dude/", "dude.babylon", this.scene, this.showReflection);
+    
+    switch (importCase) {
+      case "NurseFromDae":
+        this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, -100), this.scene);
+        BABYLON.SceneLoader.ImportMesh("", "/models/nurseFromDae/", "nurse.babylon", this.scene, this.showImportTest);
+        break;
+      default:
+        this.light = new BABYLON.PointLight("Omni", new BABYLON.Vector3(20, 20, 100), this.scene);
+        BABYLON.SceneLoader.ImportMesh("", "/models/dude/", "dude.babylon", this.scene, this.showReflection);
+    
     }
 
     engine.runRenderLoop(() => {
