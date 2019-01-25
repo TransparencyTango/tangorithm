d1f49d0ee0175f0a7fc9d66f5eec305a90712059
Eva-Maria Herbst <emherbst@web.de>
2019-01-25 15:43:24 +0100

    first autocomplete/suggestion field

--- mobile-input/src/Form.js
+++ mobile-input/src/Form.js
@@ -1,4 +1,6 @@
 import React from 'react';
+import {possibleInputs} from './possibleInputs';
+import Downshift from 'downshift';
 
 const UpperButtonRow = () => {
   const toggleMirrorView = event => {
@@ -42,8 +44,13 @@ class Form extends React.Component {
     this.handleSubmit = this.handleSubmit.bind(this);
     this.clearForm    = this.clearForm.bind(this);
     this.resetMirror   = this.resetMirror.bind(this);
+    this.onInputChoice = this.onInputChoice.bind(this);
   }
-
+  
+  onInputChoice() {
+    //debugger;
+  }
+  
   handleChange(event) {
     this.setState({
       [event.target.name]: event.target.value
@@ -70,6 +77,49 @@ class Form extends React.Component {
   render() {
     return (
       <div>
+           
+        <Downshift id="downshift-simple" itemToString={item => {return item.value;}}>
+          {({
+            getInputProps,
+            getItemProps,
+            getMenuProps,
+            highlightedIndex,
+            inputValue,
+            isOpen,
+            selectedItem,
+          }) => (
+             <div>
+              <input {...getInputProps({ placeholder: 'Search a country (start with a)'})} />
+           <div {...getMenuProps(
+                  {onChange: this.onInputChoice}
+                  )}>
+          {isOpen
+            ? possibleInputs
+                .filter(item => !inputValue || item.value.includes(inputValue))
+                .map((item, index) => (
+                  <div
+                    {...getItemProps({
+                      key: item.value,
+                      index,
+                      item,
+                      style: {
+                        backgroundColor:
+                          highlightedIndex === index ? 'lightgray' : 'white',
+                        fontWeight: selectedItem === item ? 'bold' : 'normal',
+                      },
+                    })}
+                  >
+                    {item.value}
+                  </div>
+                ))
+            : null}
+        </div>
+          </div>
+            
+            
+          )}
+        </Downshift>    
+      
       <UpperButtonRow />
       <form onSubmit={this.handleSubmit}>
       <label> Characteristics </label> <label> Hobbies </label> <br />
---
+++ mobile-input/src/possibleInputs.js
@@ -0,0 +1,9 @@
+const possibleInputs = [
+  {value: "apple"},
+  {value: 'pear'},
+  {value: 'orange'},
+  {value: 'grape'},
+  {value: 'banana'},
+]
+
+export {possibleInputs};
