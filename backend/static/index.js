0f6eaf42932efc3c03ce65ba3e3343b98782fa03
cel <cel@example.com>
2020-02-05 19:53:24 +0100

    small adjustments

--- backend/static/index.js
+++ backend/static/index.js
@@ -15,7 +15,7 @@ function submit(event) {
   input = input.split('\n');
   waitingSign.hidden = false;
   inputSumbmitButton.disabled = true;
-  
+
   // clear old values
   if (propheciesContainer.firstChild) {
     propheciesContainer.firstChild.remove();
@@ -27,8 +27,8 @@ function submit(event) {
   if (similaritiesList) {
     similaritiesContainer.removeChild(similaritiesList);
   }
-  
-  //please wait 
+
+  //please wait
 
   fetch('postAttributes?words=' + input,{
         method: "POST",
@@ -54,42 +54,43 @@ function showUpdatedResults(json) {
   let prophecies = json[0];
   ul = document.createElement('ul');
   propheciesContainer.appendChild(ul);
-  
+
   prophecies.forEach(function (item) {
     let li = document.createElement('li');
     ul.appendChild(li);
     let similarityAsPercent = ((item[1]+1)/2).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
     li.innerHTML += item[0] + " (" + (similarityAsPercent) + ")";
   });
-  
+
   // show Nearest Neighbours
   let knns = json[1];
   ul = document.createElement('ul');
   knnContainer.appendChild(ul);
-  
+
   knns.forEach(function (item) {
     let li = document.createElement('li');
     ul.appendChild(li);
     li.innerHTML += item;
   });
-  //showSimilarities  
+  //showSimilarities
   let similarityValues = json[2];
-  
+
   ulSim = document.createElement('ul');
   ulSim.setAttribute("id", similaritiesListId);
 
   similaritiesContainer.appendChild(ulSim);
-  
+
   let index = 0;
   if (similaritiesKeys.length != similarityValues.length) {
     throw "Fehler. Weniger oder Mehr Similarities als vorgesehen";
   }
-  
+
   similaritiesKeys.forEach(function (item) {
     let li = document.createElement('li');
-    li.innerHTML = item.innerHTML + ": " + similarityValues[index]
+    let similarityAsPercent = ((similarityValues[index]+1)/2).toLocaleString(undefined,{style: 'percent', minimumFractionDigits:2});
+    li.innerHTML = item.innerHTML + ": " + similarityAsPercent
     ulSim.appendChild(li);
     index +=1;
   });
-  
+
 }
